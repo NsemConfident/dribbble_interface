8ed517f27b9eab24c101689f6cdf95b5952d7a09
unknown <njocknsem@gmail.com>
2024-10-02 15:35:15 +0100

    added profile in the nav

--- src/componenets/nav/Nav.jsx
+++ src/componenets/nav/Nav.jsx
@@ -1,10 +1,20 @@
 import "./nav.css";
+import { CiEdit } from "react-icons/ci";
+import { TbMessage2 } from "react-icons/tb";
+import { FiSettings } from "react-icons/fi";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import profile from '../../../public/mypictures/mypic.jpg'
+
+
+
 
 const Nav = () => {
   return (
     <div className="nav_container">
       <div className="search_container">
-        <img src="" alt="" />
+        <div>
+        <CiEdit fontSize={20} color="black" fontWeight={500}/>
+        </div>
         <input type="search" placeholder="Search" />
       </div>
       <div className="link_container">
@@ -17,63 +27,16 @@ const Nav = () => {
         </select>
       </div>
       <div className="account_controls_container">
-        <svg
-          className="w-6 h-6 text-gray-800 dark:text-white"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          fill="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M3.559 4.544c.355-.35.834-.544 1.33-.544H19.11c.496 0 .975.194 1.33.544.356.35.559.829.559 1.331v9.25c0 .502-.203.981-.559 1.331-.355.35-.834.544-1.33.544H15.5l-2.7 3.6a1 1 0 0 1-1.6 0L8.5 17H4.889c-.496 0-.975-.194-1.33-.544A1.868 1.868 0 0 1 3 15.125v-9.25c0-.502.203-.981.559-1.331ZM7.556 7.5a1 1 0 1 0 0 2h8a1 1 0 0 0 0-2h-8Zm0 3.5a1 1 0 1 0 0 2H12a1 1 0 1 0 0-2H7.556Z"
-            clip-rule="evenodd"
-          />
-        </svg>
-        <svg
-          class="w-6 h-6 text-gray-800 dark:text-white"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M21 13v-2a1 1 0 0 0-1-1h-.757l-.707-1.707.535-.536a1 1 0 0 0 0-1.414l-1.414-1.414a1 1 0 0 0-1.414 0l-.536.535L14 4.757V4a1 1 0 0 0-1-1h-2a1 1 0 0 0-1 1v.757l-1.707.707-.536-.535a1 1 0 0 0-1.414 0L4.929 6.343a1 1 0 0 0 0 1.414l.536.536L4.757 10H4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h.757l.707 1.707-.535.536a1 1 0 0 0 0 1.414l1.414 1.414a1 1 0 0 0 1.414 0l.536-.535 1.707.707V20a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-.757l1.707-.708.536.536a1 1 0 0 0 1.414 0l1.414-1.414a1 1 0 0 0 0-1.414l-.535-.536.707-1.707H20a1 1 0 0 0 1-1Z"
-          />
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
-          />
-        </svg>
-        <svg
-          className="w-6 h-6 text-gray-800 dark:text-white"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          fill="none"
-          viewBox="0 0 24 24"
-        >
-          <path
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 5.365V3m0 2.365a5.338 5.338 0 0 1 5.133 5.368v1.8c0 2.386 1.867 2.982 1.867 4.175 0 .593 0 1.292-.538 1.292H5.538C5 18 5 17.301 5 16.708c0-1.193 1.867-1.789 1.867-4.175v-1.8A5.338 5.338 0 0 1 12 5.365ZM8.733 18c.094.852.306 1.54.944 2.112a3.48 3.48 0 0 0 4.646 0c.638-.572 1.236-1.26 1.33-2.112h-6.92Z"
-          />
-        </svg>
-        
+      <div>
+      <TbMessage2 fontSize={30}/>
+      </div>
+      <div>
+      <FiSettings fontSize={30}/>
+      </div>
+      <div>
+      <IoIosNotificationsOutline fontSize={30}/>
+      </div>
+      <img src={profile} alt="my profile picture" />
       </div>
     </div>
   );
